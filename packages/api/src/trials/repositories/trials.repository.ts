12de8d5e18b3821583483eda7372afc949de7c6b
Eduard <eduard@macbookpro.home>
2025-02-16 15:55:59 +0100

    Simplify extracting objects from trials.json

--- packages/api/src/trials/repositories/trials.repository.ts
+++ packages/api/src/trials/repositories/trials.repository.ts
@@ -1,16 +1,15 @@
 import { Injectable } from "@nestjs/common";
-import { readFile } from "fs/promises";
-import { resolve as resolvePath } from "path";
 import { TrialModel } from "../models/trial.model";
 import { TrialMapper } from "../mappers/trial.mapper";
+import { IRawTrial } from "../interfaces/raw-trial.interface";
+
+const rawTrials: IRawTrial[] = require('../../../trials.json')
 
 @Injectable()
 export class TrialsRepository {
     constructor(private trialMapper: TrialMapper) {}
 
     public async getTrials(): Promise<TrialModel[]> {
-        const rawTrials = JSON.parse(await readFile(resolvePath('./trials.json'), 'utf-8'))
-
         return rawTrials.map(this.trialMapper.map)
     }
 }
