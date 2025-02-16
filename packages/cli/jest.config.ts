519edb852b02c7616cc4684d08387d5baa402382
Eduard <eduard@macbookpro.home>
2025-02-16 18:53:30 +0100

    Test countries service

--- packages/cli/jest.config.ts
+++ packages/cli/jest.config.ts
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type {Config} from 'jest';
+import type { Config } from 'jest';
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -99,7 +99,7 @@ const config: Config = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+  preset: 'ts-jest',
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -172,7 +172,9 @@ const config: Config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+  }
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
---
+++ packages/cli/src/trials/services/country.service.spec.ts
@@ -0,0 +1,34 @@
+import { describe, expect, it, jest } from '@jest/globals';
+import { countryService } from './country.service';
+
+jest.mock('../../assets/countries.json', () => ([
+    {
+        "name": "France",
+        "code": "FR"
+    },
+    {
+        "name": "Spain",
+        "code": "ES"
+    },
+]))
+
+describe('countryService', () => {
+    const service = countryService()
+
+    it('should return a list country code options from countries.json', () => {
+        expect(service.getCountryCodeOptions()).toEqual(['FR', 'ES'])
+    })
+
+    it('should return country name based on country code', () => {
+        expect(service.getCountryName('FR')).toEqual('France')
+        expect(service.getCountryName('ES')).toEqual('Spain')
+    })
+
+    it('should throw an error if given country code does not exists', () => {
+        try {
+            expect(service.getCountryName('LA')).toThrowError(Error);
+        } catch (e: any) {
+            expect(e.message).toEqual('Country code LA does not exists')
+        }
+    })
+})
