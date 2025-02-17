09c76e3cd6bd8f688ee1319a3f006f2985455d46
Eduard <eduard@macbookpro.home>
2025-02-17 19:27:00 +0100

    create trials service

---
+++ packages/cli/src/trials/services/trials.service.ts
@@ -0,0 +1,12 @@
+import { TrialModel } from "../models/trial.model";
+import { trialsRepository } from "../repositories/trials.repository"
+
+export const trialsService = () => {
+    const getOngoingTrials = async (country?: string): Promise<TrialModel[]> => {
+        return await trialsRepository().getOngoingTrials(country);
+    }
+
+    return {
+        getOngoingTrials
+    }
+}
