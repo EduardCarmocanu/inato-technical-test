5e5ea629d9566fe15196feaa2dd7e35106435eb7
Eduard <eduard@macbookpro.home>
2025-02-17 19:26:33 +0100

    Create trials repository

---
+++ packages/cli/src/trials/repositories/trials.repository.ts
@@ -0,0 +1,24 @@
+import { TrialModel } from "../models/trial.model";
+
+const trialsApiBaseUrl = 'http://localhost:3000';
+
+export const trialsRepository = () => {
+    const getOngoingTrials = async (country?: string): Promise<TrialModel[]> => {
+        const url = new URL(`${trialsApiBaseUrl}/trials/ongoing`);
+
+        if (country) {
+            url.searchParams.set('country', country);
+        }
+
+        try {
+            const response = await fetch(url);
+            return JSON.parse(await response.text())
+        } catch (e) {
+            throw new Error('Could not retrieve ongoing trials')
+        }
+    }
+
+    return {
+        getOngoingTrials
+    }
+}
